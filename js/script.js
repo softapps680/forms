75d21289582ca43b8dbc0ee13a8cadde1245e226
paula <softapps680@gmail.com>
2021-06-01 14:19:32 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,24 +1,88 @@
 
+const login = () =>{
 
-const submitRegForm = (event) => {
-    event.preventDefault()
+        let mailformat = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        
+       
+        if(!mailformat.test(document.getElementById('user').value)|| document.getElementById('pass').value=='') {
+                formError.innerText='Nåt fält fel'
+                return false;
+        }
+       /* if(document.getElementById('pass').value=='') {
+                formError.innerText='Nåt fält fel'
+                return false;
+        }*/
+     
+                formError.innerText=''
+                return true;
 
-    // const fname = event.target['fname'].value.trim()
-    
-    console.log("HALLON "+event.target['fname'].value.trim());
-    firstNameError.innerText = 'Här är resultatet'
+       
+        
+
+}
+ 
+
+
+
+
+
+const validate = (event) =>{
+        
+        let mailformat = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        
+      if(event.target.id=='fname' && event.target.value.trim().length<2 ) {
+                fnameError.innerText = 'För lite antal tecken' 
+                return false;  
+                 }
+        else fnameError.innerText = '' 
+       if(event.target.id=='lname' && event.target.value.trim().length<2) {
+                lnameError.innerText = 'För lite antal tecken'
+                return false;  
+        } 
+        else lnameError.innerText = ''
+        if(event.target.id=='tel'){
+                if(isNaN(event.target.value)) {        
+                telError.innerText = 'Siffror i fältet' 
+                return false;
+                } 
+                else telError.innerText = '' 
+        }
+        if(event.target.id=='epost'){
+                if(!mailformat.test(event.target.value)){
+                regepostError.innerText='ogiltig e-post'
+                return false;
+                }
+                else  regepostError.innerText=''
+        }  
+        if(event.target.id=='confirm_password'){
+                var password = document.getElementById("password").value;
+                var confirmPassword = document.getElementById("confirm_password").value;
+               
+                if (password != confirmPassword) {
+                        losen2Error.innerText='Lösenorden matchar inte'
+                        document.getElementById("savebtn").disabled = true;
+                    return false;
+                }
+              else losen2Error.innerText=''
+        }
+        
+        else { return true;}
+                
+        document.getElementById("savebtn").disabled = false;
+        
+             
 }
 
-const validate = (value) =>{
-   
-    lastNameError.innerText = 'obsobs' + value;
-}
+
+
 
 const start = () =>{
+   
     var login = document.getElementById("login");
     var reg = document.getElementById("register");
     reg.style.display = "none";
     login.style.display = "block";
+   
 }
 const newUser = () =>{
     var login = document.getElementById("login");
@@ -26,5 +90,4 @@ const newUser = () =>{
     login.style.display = "none";
     reg.style.display="block";
 
-   
 }
